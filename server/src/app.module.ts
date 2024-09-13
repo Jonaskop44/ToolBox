e47660990e7e67b3ad52d3de0a52827d36198772
Jonaskop44 <jonas@codeflexx.com>
2024-09-13 22:00:45 +0200

    changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UserModule],
   controllers: [],
   providers: [PrismaService],
 })
---
+++ server/src/user/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id?: number;
+  username: string;
+  password: string;
+  email: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
