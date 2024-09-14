b9f241d8a978b076b8934eff9a490d2f47be1fc8
Jonaskop44 <jonas@codeflexx.com>
2024-09-14 12:51:31 +0200

    changes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -23,7 +23,7 @@ export class AuthController {
   }
 
   @UseGuards(RefreshJwtGuard)
-  @Post('refresh-token')
+  @Post('refreshToken')
   async refreshToken(@Request() request) {
     return this.authService.refreshToken(request.user);
   }
