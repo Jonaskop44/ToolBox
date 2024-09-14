c4704083f0ff7678f499db89d1036ccc7b4415c8
Jonaskop44 <jonas@codeflexx.com>
2024-09-14 12:28:31 +0200

    changes

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/auth.dto';
+import { compare } from 'bcrypt';
+
+const EXPIRE_TIME = 1000 * 60 * 60 * 24;
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(dto: LoginDto) {
+    const user = await this.userService.findUserByEmail(dto.email);
+    if (user && (await compare(dto.password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.validateUser(dto);
+    const payload = {
+      email: user.email,
+      sub: {
+        name: user.name,
+      },
+    };
+
+    return {
+      user,
+      backendTokens: {
+        accessToken: await this.jwtService.signAsync(payload, {
+          expiresIn: '1d',
+          secret: process.env.JWT_SECRET,
+        }),
+        refreshToken: await this.jwtService.signAsync(payload, {
+          expiresIn: '7d',
+          secret: process.env.JWT_REFRESH_TOKEN,
+        }),
+        expiresIn: new Date().setTime(new Date().getTime() + EXPIRE_TIME),
+      },
+    };
+  }
+
+  async refreshToken(user: any) {
+    const payload = {
+      email: user.email,
+      sub: user.sub,
+    };
+
+    return {
+      accessToken: await this.jwtService.signAsync(payload, {
+        expiresIn: '1d',
+        secret: process.env.JWT_SECRET,
+      }),
+      refreshToken: await this.jwtService.signAsync(payload, {
+        expiresIn: '7d',
+        secret: process.env.JWT_REFRESH_TOKEN,
+      }),
+      expiresIn: new Date().setTime(new Date().getTime() + EXPIRE_TIME),
+    };
+  }
+}
