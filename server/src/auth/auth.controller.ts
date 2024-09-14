cef76001067b7c421353c8202f530461061cfcd1
Jonaskop44 <jonas@codeflexx.com>
2024-09-14 12:38:15 +0200

    changes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dto/user.dto';
+import { LoginDto } from './dto/auth.dto';
+import { UserService } from 'src/user/user.service';
+import { RefreshJwtGuard } from 'src/guard/refresh.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('register')
+  async registerUser(@Body() dto: CreateUserDto) {
+    return this.userService.createUser(dto);
+  }
+
+  @Post('login')
+  async loginUser(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @UseGuards(RefreshJwtGuard)
+  @Post('refresh-token')
+  async refreshToken(@Request() request) {
+    return this.authService.refreshToken(request.user);
+  }
 }
