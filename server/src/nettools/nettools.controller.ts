67fc1e78af04068f8bb9d4db09dcf61f50cea2c3
Jonaskop44 <jonas@codeflexx.com>
2024-09-22 15:29:15 +0200

    changes

--- server/src/nettools/nettools.controller.ts
+++ server/src/nettools/nettools.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { NettoolsService } from './nettools.service';
 
 @Controller('nettools')
 export class NettoolsController {
   constructor(private readonly nettoolsService: NettoolsService) {}
+
+  @Get('ipinfo/:ip')
+  async getIpInfo(@Param('ip') ip: string) {
+    return this.nettoolsService.getIpInfo(ip);
+  }
 }
--- server/src/nettools/nettools.service.ts
+++ server/src/nettools/nettools.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class NettoolsService {}
+export class NettoolsService {
+  constructor() {}
+
+  async getIpInfo(ip: string) {
+    return axios
+      .get(`https://www.ipinfo.io/${ip}/json?token=${process.env.IPINFO_TOKEN}`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        throw new ConflictException(
+          'Something went wrong while fetching IP info',
+        );
+      });
+  }
+}
