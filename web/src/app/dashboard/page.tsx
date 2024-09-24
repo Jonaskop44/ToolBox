74be3253b38188f8878edd64187fd0fe7da600a9
Jonaskop44 <jonas@codeflexx.com>
2024-09-24 17:49:54 +0200

    changes

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -5,8 +5,8 @@ import Particels from "@/components/Common/Particels";
 const Dashboard = () => {
   return (
     <div className="bg-image m-0 h-full w-full">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/65">
-        <h1>hallo</h1>
+      <div className="flex justify-center items-center h-screen bg-black/65">
+        <h1 className="text-center text-white font-bold text-6xl">Toolbox</h1>
         <Particels />
       </div>
     </div>
