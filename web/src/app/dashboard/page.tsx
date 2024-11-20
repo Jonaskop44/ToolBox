d45b638ca46f2d8eb26a3dff6bf261e183a3a025
Fenninger <marco.fenninger@edu.htl-villach.at>
2024-11-20 13:03:19 +0100

    changes

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,34 +1,41 @@
 "use client";
-
-import Particels from "@/components/Common/Particels";
-import { AiOutlineDiscord } from "react-icons/ai";
 import DashboardCard from "@/components/DashboardCard";
+import Particels from "@/components/Common/Particels";
 import { VscTools } from "react-icons/vsc";
 import { SlCloudDownload } from "react-icons/sl";
+import { AiOutlineDiscord } from "react-icons/ai";
+
+
 
 const cards = [
-  { title: "Discord", description: "Join our Discord!", icon: AiOutlineDiscord },
-  { title: "Downloader", description: "Follow us on Twitter!", icon: SlCloudDownload },
-  { title: "Nettools", description: "Follow us on Twitter!", icon: VscTools },
+  { title: "Nettools", description: "Portscan, IP-Info und Mac Adressen-Info", icon: VscTools },
+  { title: "Downloader", description: "Youtube und Spotify", icon: SlCloudDownload },
+  { title: "Discord", description: "Discordnuker", icon: AiOutlineDiscord },
 ];
 
 const Dashboard = () => {
   return (
-    <div className="bg-gray-800 m-0 min-h-screen w-full flex flex-col items-center relative">
+    <div className="bg-gray-900 m-0 min-h-screen w-full flex flex-col items-center relative">
       <div className="absolute top-10 w-full flex justify-center z-10">
         <h1 className="text-center text-white font-bold text-6xl">Toolbox 360</h1>
       </div>
-      <div className="flex justify-center gap-8 mt-40 w-11/12 max-w-5xl z-10"> 
-      {cards.map((card) => (
-        <DashboardCard
-          key={card.title}
-          title={card.title}
-          description={card.description}
-          icon={card.icon}
-        />
-      ))}
+      
+      
+      <div className="flex justify-center gap-8 mt-40 w-11/12 max-w-5xl z-10">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-8 w-full">
+          {cards.map((card) => (
+            <DashboardCard
+              key={card.title}
+              title={card.title}
+              description={card.description}
+              icon={card.icon}
+            />
+          ))}
+        </ul>
       </div>
-      <Particels /> 
+      
+      
+      <Particels />
     </div>
   );
 };
--- web/src/components/DashboardCard.tsx
+++ web/src/components/DashboardCard.tsx
@@ -9,26 +9,18 @@ interface DashboardCardProps {
 
 const DashboardCard: React.FC<DashboardCardProps> = ({ title, description, icon: Icon }) => {
   return (
-    <ul
-      role="list"
-      className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-    >
-      <li
-        className="col-span-1 w-80 flex flex-col text-center bg-gray-400 rounded-lg shadow-lg divide-y divide-gray-200 
-                     hover:shadow-2xl hover:-translate-y-2 transition duration-300 ease-in-out"
-      >
-        <div className="flex-1 flex flex-col p-6">
-          <div className="w-32 h-32 mx-auto flex items-center justify-center rounded-full bg-white">
-            <Icon className="text-gray-900" size={60} />
-          </div>
-          <h3 className="mt-6 text-gray-900 text-lg font-medium">{title}</h3>
-          <dl className="mt-1 flex-grow flex flex-col justify-between">
-            <dt className="sr-only">Title</dt>
-            <dd className="text-gray-900 text-sm">{description}</dd>
-          </dl>
+    <li className="w-full sm:w-80 flex flex-col text-center bg-gray-800 rounded-lg shadow-lg divide-y divide-gray-200 border border-[#38bdf8] hover:shadow-2xl hover:-translate-y-2 transition duration-300 ease-in-out">
+      <div className="flex-1 flex flex-col p-6">
+        <div className="w-32 h-32 mx-auto flex items-center justify-center rounded-full bg-[#38bdf8]">
+          <Icon className="text-gray-900" size={60} />
         </div>
-      </li>
-    </ul>
+
+        <h3 className="mt-6 text-white text-lg font-medium font-serif">{title}</h3>
+        <div className="mt-1 flex-grow flex flex-col justify-between">
+          <p className="text-white text-sm font-sans">{description}</p>
+        </div>
+      </div>
+    </li>
   );
 };
 
