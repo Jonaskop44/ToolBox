0ac7a8114663bfebea8acefbb22788ffdba0bad5
jonaskop44 <jonas@codeflexx.com>
2024-11-20 13:43:21 +0100

    changes

--- web/src/components/Common/Loader.tsx
+++ web/src/components/Common/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-[#f3f4f6] dark:bg-dark">
+    <div className="flex h-screen items-center justify-center bg-gray-900">
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
