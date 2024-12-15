9a67ae8e56bfb72de6698e92224e6dddd5d7d08b
jonaskop44 <jonas@codeflexx.com>
2024-12-15 16:58:02 +0100

    changes

---
+++ web/src/api/discord/index.ts
@@ -0,0 +1,8 @@
+import Helper from "./helper";
+
+export class Discord {
+  helper: Helper;
+  constructor() {
+    this.helper = new Helper();
+  }
+}
--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 import { Auth } from "./auth";
+import { Discord } from "./discord";
 
 export default class ApiClient {
   auth: Auth;
+  discord: Discord;
   constructor() {
     this.auth = new Auth();
+    this.discord = new Discord();
 
     axios.defaults.headers.common["Authorization"] = `Bearer ${Cookies.get(
       "accessToken"
